521a4ac3b54881ee2e2bebb11c4d87bb3a09e92e
11SID22 <sid111097@gmail.com>
2023-10-20 17:37:59 +0530

    learned get elements by class name

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 // console.log('hello World');
 
-
+// Get element by ID
 let headerTitle = document.getElementById("header-title");
 // console.log(headerTitle);
 
 headerTitle.style.border = 'solid 2px #000'; 
 
 
-let title = document.getElementById("main");
-console.log(title);
+// let title = document.getElementById("main");
+// console.log(title);
 
 // title.textContent = 'sssss';
-title.style.color = 'green';
+// title.style.color = 'green';
+
+// Get elements by class name
+let items = document.getElementsByClassName('list-group-item')
+// console.log(item);
+
+items[2].style.backgroundColor = 'green';
+
+for(let i=0; i<items.length; i++)[
+    items[i].style.fontWeight = 'bold'
+]
