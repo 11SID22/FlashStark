731899962034c848bec9efe8b2c6b21a2e6f1ac4
11SID22 <sid111097@gmail.com>
2023-10-20 16:40:50 +0530

    added 1st task in new branch

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hello World');
+// console.log('hello World');
+
+
+let headerTitle = document.getElementById("header-title");
+// console.log(headerTitle);
+
+headerTitle.style.border = 'solid 2px #000'; 
+
+
+let title = document.getElementById("main");
+console.log(title);
+
+// title.textContent = 'sssss';
+title.style.color = 'green';
