fc5fbfaa61d2c7c9c5064cb68847d5a389b87eb8
11SID22 <sid111097@gmail.com>
2023-10-20 17:50:44 +0530

    added get elements by tag name

--- app.js
+++ app.js
@@ -22,3 +22,12 @@ items[2].style.backgroundColor = 'green';
 for(let i=0; i<items.length; i++)[
     items[i].style.fontWeight = 'bold'
 ]
+
+// Get elements by tag name
+let li = document.getElementsByTagName('li');
+// console.log(li);
+
+li[1].style.backgroundColor = 'red';
+
+// items[4].style.backgroundColor = 'Yellow'; // Tried by class name which is not assigned to item 5.
+li[4].style.backgroundColor = 'Yellow';
