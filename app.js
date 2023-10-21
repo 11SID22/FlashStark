fe5ce9443770ba842a44266c535a0d36b81cd902
11SID22 <sid111097@gmail.com>
2023-10-21 14:36:52 +0530

    Creating nodes and modifying DOM

--- app.js
+++ app.js
@@ -52,13 +52,88 @@
 
 
 // QUERYSELECTORALL //
-let secondItem = document.querySelectorAll('.list-group-item');
-console.log(secondItem);
+// let secondItem = document.querySelectorAll('.list-group-item');
+// console.log(secondItem);
 
-secondItem[1].style.color = 'green';
+// secondItem[1].style.color = 'green';
 
-let odd = document.querySelectorAll('li:nth-child(odd)');
+// let odd = document.querySelectorAll('li:nth-child(odd)');
 
-for(let i=0; i<odd.length; i++){
-    odd[i].style.backgroundColor = 'green';
-}
+// for(let i=0; i<odd.length; i++){
+//     odd[i].style.backgroundColor = 'green';
+// }
+
+// TRAVERSING THE DOM //
+
+// Parent node 
+// let itemList = document.querySelector('#items');
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+
+// Parent element
+// let itemList = document.querySelector('#items');
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#000000';
+
+// Last element child
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.style.backgroundColor = '#999fff'
+
+// First element child
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello World';
+
+// Last child
+// console.log(itemList.lastChild);
+
+// First child
+// console.log(itemList.firstChild);
+
+// Next sibling
+// console.log(itemList.nextSibling);
+
+// Previous sibling
+// console.log(itemList.previousSibling);
+
+// Next element sibling
+// console.log(itemList.nextElementSibling);
+
+// Previous element sibling 
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = "green";
+
+// Create element
+
+// Create a div
+let newDiv = document.createElement('div');
+
+
+// Add class
+newDiv.className = 'hello';
+
+// Add id
+newDiv.id = 'hello1';
+
+// Set attribute
+newDiv.setAttribute('title', 'hello div');
+
+// Create a text node
+let newDivText = document.createTextNode('Hello World');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+console.log(newDiv);
+
+// Add this newDiv to the dom
+let container = document.querySelector('header .container');
+let h1 = document.querySelector('header h1');
+
+container.insertBefore(newDiv, h1);
+
+
+let itemList = document.getElementById('items');
+itemList.innerHTML = '<li>Hello WORLD</li>' + itemList.innerHTML
+
+
+
