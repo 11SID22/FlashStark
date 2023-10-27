b2804482ad89abb4218f323c88f489d3a845a15c
11SID22 <sid111097@gmail.com>
2023-10-27 10:27:03 +0530

    aaaa

--- app.js
+++ app.js
@@ -105,35 +105,60 @@
 // Create element
 
 // Create a div
-let newDiv = document.createElement('div');
+// let newDiv = document.createElement('div');
 
 
-// Add class
-newDiv.className = 'hello';
+// // Add class
+// newDiv.className = 'hello';
 
-// Add id
-newDiv.id = 'hello1';
+// // Add id
+// newDiv.id = 'hello1';
 
-// Set attribute
-newDiv.setAttribute('title', 'hello div');
+// // Set attribute
+// newDiv.setAttribute('title', 'hello div');
 
-// Create a text node
-let newDivText = document.createTextNode('Hello World');
+// // Create a text node
+// let newDivText = document.createTextNode('Hello World');
 
-// Add text to div
-newDiv.appendChild(newDivText);
+// // Add text to div
+// newDiv.appendChild(newDivText);
 
-console.log(newDiv);
+// console.log(newDiv);
 
-// Add this newDiv to the dom
-let container = document.querySelector('header .container');
-let h1 = document.querySelector('header h1');
+// // Add this newDiv to the dom
+// let container = document.querySelector('header .container');
+// let h1 = document.querySelector('header h1');
 
-container.insertBefore(newDiv, h1);
+// container.insertBefore(newDiv, h1);
 
 
+// let itemList = document.getElementById('items');
+// itemList.innerHTML = '<li>Hello WORLD</li>' + itemList.innerHTML
+
+
+
+//Add and Delete Items
+let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
-itemList.innerHTML = '<li>Hello WORLD</li>' + itemList.innerHTML
 
+// Form submit event
+form.addEventListener('submit', addItem);
 
+// Add item
+function addItem(e){ 
+    e.preventDefault();
+}
 
+// Get input value 
+let newItem = document.getElementById('item').value;
+
+// create new li element
+let li = document.createElement('li');
+
+// Add class (same as other li class)
+li.className = 'list-group-item';
+
+// Add text node with input value
+li.appendChild(document.createTextNode(newItem));
+
+itemList.appendChild(li);
